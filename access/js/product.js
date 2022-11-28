b433d659ddd736790b245d1d19efc66127690c7d
vanhieu154 <117420642+vanhieu154@users.noreply.github.com>
2022-11-28 21:28:31 +0700

    Làm gọn chức năng lọc và thêm tính tăng tìm kiếm

--- access/js/product.js
+++ access/js/product.js
@@ -8,172 +8,148 @@ function LoadJson(){
     })
     .then(
         function(data){
-        let html ="";
-        for(let p of data){
-            function showProduct(p) {
-                var s = 0;
-                s=p.Price-p.Discount*p.Price/100;
-                let price ="";
-                let discountTag="";
-                if (p.Discount==0) {
-                    price+="<div class='home-product-item__price'>";
-                    price+="<span class='home-product-item__price-no-discount'>"+p.Price+".000đ</span>";
-                    price+="</div>";
-                }else{
-                    price+="<div class='home-product-item__price'>";
-                    price+="<span class='home-product-item__price-current'>"+s+".000đ</span>";
-                    price+="<span class='home-product-item__price-old'>"+p.Price+".000đ</span>";
-                    price+="</div>";
-                    
-                    discountTag+="<div class='home-product-item__sale-off'>";
-                    discountTag+="<div class='home-product-item__sale-off-percent'>"+p.Discount+"%</div>";
-                    discountTag+="<div class='home-product-item__sale-off-label'>GIẢM</div>";
-                    discountTag+="</div>";
+            function showProduct(arrP,start,end,place) {
+                let html ="";
+                for(i=start;i<end;i++){
+                    if(i>=arrP.length) break;
+                    var s = 0;
+                    s=arrP[i].Price-arrP[i].Discount*arrP[i].Price/100;
+                    let price ="";
+                    let discountTag="";
+                    if (arrP[i].Discount==0) {
+                        price+="<div class='home-product-item__price'>";
+                        price+="<span class='home-product-item__price-no-discount'>"+arrP[i].Price+".000đ</span>";
+                        price+="</div>";
+                    }else{
+                        price+="<div class='home-product-item__price'>";
+                        price+="<span class='home-product-item__price-current'>"+s+".000đ</span>";
+                        price+="<span class='home-product-item__price-old'>"+arrP[i].Price+".000đ</span>";
+                        price+="</div>";
+                        
+                        discountTag+="<div class='home-product-item__sale-off'>";
+                        discountTag+="<div class='home-product-item__sale-off-percent'>"+arrP[i].Discount+"%</div>";
+                        discountTag+="<div class='home-product-item__sale-off-label'>GIẢM</div>";
+                        discountTag+="</div>";
+                    }
+                    html+="<div class='col-xl-2 col-lg-3 col-4 product' id='"+arrP[i].MaSP+"'>";
+                    html+="<button class='home-product-item'  type='submit' name='maSp' id='"+arrP[i].MaSP+"' value='"+arrP[i].MaSP+"'>";
+                    html+="<div class='home-product-item__img' style='background-image:url(./access/Img/sanpham/"+arrP[i].Hinhanh[0]+")'></div>";
+                    html+="<h4 class='home-product-item__name'>"+arrP[i].TenSP+"</h4>";
+                    html+=price;
+                    html+="<div class='home-product-item__action'>";
+                    html+="<span class='home-product-item__like home-product-item__like--liked'>";
+                    html+="<i class='home-product-item__like-icon-emty fa-regular fa-heart'></i>";
+                    html+="<i class='home-product-item__like-icon-fil fa-solid fa-heart'></i>";
+                    html+="</span>";
+                    html+="</div>";
+                    html+="<div class='home-product-item__origin'>";
+                    html+="<div class='home-product-item__grand'>"+arrP[i].Hang+"</div>";
+                    html+="<div class='home-product-item__origin-name'>Nhật bản</div>";
+                    html+="</div>";
+                    html+="<div class='home-product-item__favor'>";
+                    html+="<i class='fa-solid fa-check'></i>";
+                    html+="<span>Yêu thích</span>";
+                    html+="</div>";
+                    html+=discountTag;
+                    html+="</button>";
+                    html+="</div>";
+                    document.getElementById(place).innerHTML=html;
                 }
-                html+="<div class='col-xl-2 col-lg-3 col-4 product' id='"+p.MaSP+"'>";
-                html+="<button class='home-product-item'  type='submit' name='maSp' id='"+p.MaSP+"' value='"+p.MaSP+"'>";
-                html+="<div class='home-product-item__img' style='background-image:url(./access/Img/sanpham/"+p.Hinhanh[0]+")'></div>";
-                html+="<h4 class='home-product-item__name'>"+p.TenSP+"</h4>";
-                html+=price;
-                html+="<div class='home-product-item__action'>";
-                html+="<span class='home-product-item__like home-product-item__like--liked'>";
-                html+="<i class='home-product-item__like-icon-emty fa-regular fa-heart'></i>";
-                html+="<i class='home-product-item__like-icon-fil fa-solid fa-heart'></i>";
-                html+="</span>";
-                html+="</div>";
-                html+="<div class='home-product-item__origin'>";
-                html+="<div class='home-product-item__grand'>"+p.Hang+"</div>";
-                html+="<div class='home-product-item__origin-name'>Nhật bản</div>";
-                html+="</div>";
-                html+="<div class='home-product-item__favor'>";
-                html+="<i class='fa-solid fa-check'></i>";
-                html+="<span>Yêu thích</span>";
-                html+="</div>";
-                html+=discountTag;
-                html+="</button>";
-                html+="</div>";
-                document.getElementById("total_product").innerHTML=html;
             }
-            showProduct(p)
+            showProduct(data,0,data.length,"total_product");
             const typeCategorys = document.querySelectorAll(".type_category");
             for(let i=0; i<typeCategorys.length;i++){
                 typeCategorys[i].onclick=function(){
-                    document.getElementById("total_product").innerHTML="";
-                    html="";
-                    for(let p of data){
+                    product=[];
+                    j=0;
+                    var ma="";
                         switch (i){
                             case 0: 
-                                if(p.LoaiSP =='Váy'){
-                                    showProduct(p)
-                                    // showPPdetail()
-
-                                }  
+                                ma ='Váy'
                                 break;
                             case 1: 
-                                if(p.LoaiSP =='Áo'){
-                                    showProduct(p)
-                                    // showPPdetail()                                    
-                                }  
+                                ma ='Áo'
                                 break;
                             case 2: 
-                                if(p.LoaiSP =='Phụ kiện'){
-                                    showProduct(p)
-                                    // showPPdetail()                                    
-            
-
-                                }  
+                                ma ='Phụ kiện'
                                 break;
                             case 3: 
-                                if(p.LoaiSP =='Quần'){
-                                    showProduct(p)
-                                    // showPPdetail()                                    
-
-                                }  
-                                break;
-                        }
-                    }
+                                ma ='Quần'
+                                break;}
+                    for(let p of data){
+                        if(p.LoaiSP == ma){
+                            product[j]=p;
+                            j++
+                        }}
+                        showProduct(product,0,product.length,"total_product");    
                 }
             }
-
             const btnCategory = document.getElementById("btn_list");
             btnCategory.onclick=function() {
-                document.getElementById("total_product").innerHTML="";
-                html="";
+                product=[];
+                j=0;
                 let all_brand_check = document.querySelectorAll('input[class="brand_category"]:checked');
                 let all_price_check = document.querySelectorAll('input[class="price_category"]:checked');
+                if(all_brand_check.length == 0){
+                    for(let y = 0 ; y<all_price_check.length;y++){
+                        let gia=all_price_check[y].value;
+                        let s = 0;
+                        for(let p of data){
+                            s=p.Price-p.Discount*p.Price/100;
+                            if (s>(gia-100) && s<gia || s==gia) {
+                                product[j]=p;
+                                j++;  
+                            }
+                        }
+                    }
+                }else
                 for(let x = 0; x < all_brand_check.length;  x++)
                 {
                     let brand=all_brand_check[x].value;
                     for(let p of data ){
+                        if(all_price_check.length==0){
+                            if(p.Hang==brand){
+                                product[j]=p;
+                                j++;   
+                            }
+                        }
                         if (p.Hang==brand) {
                             for(let y = 0 ; y<all_price_check.length;y++){
                                 let gia=all_price_check[y].value;
                                 let s = 0;
                                 s=p.Price-p.Discount*p.Price/100;
                                 if (s>(gia-100) && s<gia || s==gia) {
-                                    showProduct(p);
+                                    product[j]=p;
+                                    j++;  
                                 }
                             }
-                        }
+                        };
                     }
                 }
-                // showPPdetail()
+                if(product.length==0){
+                    document.getElementById("total_product").innerHTML="<h1 style='margin: auto;padding-top:50px;'>Không tìm thấy sản phẩm phù hợp</h1>";
+                }else{
+                showProduct(product,0,product.length,"total_product"); 
+                }  
             }
-            // console.log(showPdetail.item(0));
-            // function showPPdetail(){
-            //     const showPdetail=document.querySelectorAll(".product")
-            //     for (let i = 0; i < showPdetail.length; i++) {
-            //         let items="";
-            //         let slider="";
-            //         showPdetail[i].onclick=function(){
-            //             // console.log(showPdetail[i]);
-            //             for(let p of data){
-            //                 if(p.MaSP == i+1){
-            //                     items+="<div class='item'  data-bs-target='#carouselExampleDark' data-bs-slide-to='0' aria-current='true' aria-label='Slide 1' alt=''>";
-            //                     items+="<img class='w-100' src='./access/Img/sanpham/"+p.Hinhanh[0]+"' />";
-            //                     items+="</div>";
-
-            //                     slider+="<div class='carousel-item active' data-bs-interval='5000'>";
-            //                     slider+="<img src='./access/Img/sanpham/"+p.Hinhanh[0]+"' class='d-block w-100' alt=''>";
-            //                     slider+="</div>"
-            //                     for (let a = 1; a < p.Hinhanh.length; a++) {
-            //                         items+="<div class='item '  data-bs-target='#carouselExampleDark' data-bs-slide-to='"+a+"' aria-label='Slide "+(a+1)+"' alt=''>";
-            //                         items+="<img class='w-100 ' src='./access/Img/sanpham/"+p.Hinhanh[a]+"' />"; 
-            //                         items+="</div>";
-
-            //                         slider+="<div class='carousel-item' data-bs-interval='5000'>";
-            //                         slider+="<img src='./access/Img/sanpham/"+p.Hinhanh[a]+"' class='d-block w-100' alt=''>";
-            //                         slider+="</div>";
-            //                     }
-            //                     document.getElementById("items").innerHTML=items;
-            //                     document.getElementById("slider").innerHTML=slider;
-            //                 }
-            //             }
-            //         }
-                    
-            //     }
-            // }
-            // showPPdetail()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        }
+            function searchProduct(){
+                var arr=data;
+                var s="";
+                product =[];
+                var j=0;
+                function getUrlParams() {
+                    const params = new URLSearchParams (window.location.search);
+                    return params;
+                }
+                let searchProduct=getUrlParams().get("search-bar__input");
+                for (i = 0; i < arr.length; i++)
+                if ((arr[i].TenSP).toLowerCase().search(searchProduct.toLowerCase()) != -1) {
+                    product[j] = arr[i];
+                    j++;
+                }
+                showProduct(product,0,product.length,"total_product")
+            }
+            searchProduct() 
         }
     )
     .catch(function(err){
