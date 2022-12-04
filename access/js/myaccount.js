bd69c1778286b5e1de550861b6ec5f186203cee9
Thanhthuyweb1 <119403013+Thanhthuyweb1@users.noreply.github.com>
2022-12-04 14:36:37 +0700

    update

--- access/js/myaccount.js
+++ access/js/myaccount.js
@@ -4,6 +4,33 @@ if(butTon){
         document.querySelector(".btn-content").classList.toggle(("activeB"))
     })
 }
-function showProfile(){
     
+const hoso=document.querySelector(".hoso");
+const diachi=document.querySelector(".diachi");
+const matkhau=document.querySelector(".doi-mat-khau");
+if(hoso){
+    hoso.addEventListener("click",function(){
+        document.querySelector(".myaccount-body-right-ho-so-cua-toi").style.display="block"
+        document.querySelector(".address").style.display="none"
+        document.querySelector(".body__container-doi-mat-khau").style.display="none"
+
+    })
 }
+if(diachi){
+    diachi.addEventListener("click",function(){
+        document.querySelector(".myaccount-body-right-ho-so-cua-toi").style.display="none"
+        document.querySelector(".address").style.display="block"
+        document.querySelector(".body__container-doi-mat-khau").style.display="none"
+
+    })
+}
+if(matkhau){
+    matkhau.addEventListener("click",function(){
+        document.querySelector(".myaccount-body-right-ho-so-cua-toi").style.display="none"
+        document.querySelector(".address").style.display="none"
+        document.querySelector(".body__container-doi-mat-khau").style.display="block"
+
+    })
+}
+
+
